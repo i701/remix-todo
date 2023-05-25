980c91217f50df9c757245f1620cd5053e0b0799
i701 <idhaan701@outlook.com>
2023-05-25 20:29:29 +0500

    switch statements and scoped data validation

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,11 +3,18 @@ import type {
   LoaderFunction,
   V2_MetaFunction,
 } from "@remix-run/node"
-import { redirect, json } from "@remix-run/node"
-import { Button, Input, Typography } from "@material-tailwind/react"
-import { Form, useActionData, useLoaderData } from "@remix-run/react"
+import { json } from "@remix-run/node"
+import { Button, Input, Spinner, Typography } from "@material-tailwind/react"
+import {
+  Form,
+  useActionData,
+  useLoaderData,
+  useNavigation,
+} from "@remix-run/react"
 import { db } from "~/utils/db.server"
 import { badRequest } from "~/utils/request.server"
+import { useEffect, useRef } from "react"
+import TodoItem from "~/components/Todo"
 
 export const meta: V2_MetaFunction = () => {
   return [
@@ -30,31 +37,63 @@ function validateTodo(content: string) {
 
 export const action: ActionFunction = async ({ request }) => {
   const form = await request.formData()
-  const content = form.get("content")
-  if (typeof content !== "string") {
-    return badRequest({
-      fieldErrors: null,
-      fields: null,
-      formError: "Form not submitted correctly.",
-    })
-  }
-  const fieldErrors = {
-    content: validateTodo(content),
-  }
-  const fields = { content }
-  console.log(fields)
-  if (Object.values(fieldErrors).some(Boolean)) {
-    return badRequest({
-      fieldErrors,
-      fields,
-      formError: null,
-    })
-  }
+  const content = form.get("content") as string
+  const _action = form.get("_action") as string
+  const todoId = form.get("todoId") as string
+  console.log({ todoId, _action })
 
-  const todo = await db.todo.create({
-    data: fields,
-  })
-  return redirect("/todos")
+  switch (_action) {
+    case "delete": {
+      const todoToDelete = await db.todo.delete({
+        where: {
+          id: todoId,
+        },
+      })
+      return todoToDelete
+    }
+    case "complete": {
+      const isCompleted = !!form.get("isCompleted")
+      console.log({ isCompleted })
+
+      const updateTodo = await db.todo.update({
+        where: {
+          id: todoId,
+        },
+        data: {
+          isComplete: isCompleted,
+        },
+      })
+      return updateTodo
+    }
+    case "create": {
+      if (typeof content !== "string") {
+        return badRequest({
+          fieldErrors: null,
+          fields: null,
+          formError: "Form not submitted correctly.",
+        })
+      }
+      const fieldErrors = {
+        content: validateTodo(content),
+      }
+      const fields = { content }
+      console.log(fields)
+      if (Object.values(fieldErrors).some(Boolean)) {
+        return badRequest({
+          fieldErrors,
+          fields,
+          formError: null,
+        })
+      }
+
+      const newTodo = await db.todo.create({
+        data: fields,
+      })
+      return newTodo
+    }
+    default:
+      return null
+  }
 }
 
 export const loader: LoaderFunction = async () => {
@@ -65,22 +104,41 @@ export const loader: LoaderFunction = async () => {
 export default function Index() {
   const todos = useLoaderData<TodoType[] | []>()
   const actionData = useActionData<typeof action>()
+  const formRef = useRef<HTMLFormElement>(null)
+  const todoInputRef = useRef<HTMLInputElement>(null)
+  const navigation = useNavigation()
+  const isAdding =
+    navigation.state === "submitting" &&
+    navigation.formData.get("_action") === "create"
+
+  useEffect(() => {
+    formRef?.current?.reset()
+  }, [isAdding])
+
   return (
     <div>
       <Typography variant="h3" color="blue-gray" className="py-4">
         Todos y'all!
       </Typography>
-      <div>
-        {todos.map((todo) => (
-          <p key={todo.id}>{todo.content}</p>
-        ))}
+      <div className="space-y-1">
+        {todos.length > 0
+          ? todos.map((todo) => (
+              <TodoItem
+                content={todo.content}
+                key={todo.id}
+                id={todo.id}
+                isComplete={todo.isComplete}
+              />
+            ))
+          : "No todos yet"}
       </div>
-      <Form className="space-y-2 py-4" method="POST">
+      <Form ref={formRef} className="space-y-2 py-4" method="post">
         <Input
+          ref={todoInputRef}
           required
           label="Todo"
           name="content"
-          error={actionData?.fieldErrors?.content}
+          error={Boolean(actionData?.fieldErrors?.content)}
           defaultValue={actionData?.fields?.content}
           aria-invalid={Boolean(actionData?.fieldErrors?.content)}
           aria-errormessage={
@@ -92,8 +150,8 @@ export default function Index() {
             {actionData.fieldErrors.content}
           </p>
         ) : null}
-        <Button type="submit" className="">
-          Button
+        <Button name="_action" value="create" type="submit">
+          {isAdding ? <Spinner className="h-4 w-4" /> : "Add"}
         </Button>
       </Form>
     </div>
