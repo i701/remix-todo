a52a5f6ad152e33fadd2c7d154f5af822e99cf16
i701 <idhaan701@outlook.com>
2023-05-27 21:30:16 +0500

    add alert component

---
+++ app/components/Alert.tsx
@@ -0,0 +1,22 @@
+import { useState, Fragment } from "react"
+import { Alert, Button } from "@material-tailwind/react"
+
+export default function AlertComponent({ error }: { error: string }) {
+  const [open, setOpen] = useState(true)
+
+  return (
+    <Fragment>
+      <Alert
+        color="red"
+        open={open}
+        onClose={() => setOpen(false)}
+        animate={{
+          mount: { y: 0 },
+          unmount: { y: 100 },
+        }}
+      >
+        {error}
+      </Alert>
+    </Fragment>
+  )
+}
